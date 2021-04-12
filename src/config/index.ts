cfa8496a08ee1e3aa3deb5b93941c0e4b8114ffd
stableflect <paul@stableflect.finance>
2021-04-12 18:24:00 +0200

    merge marker removed, version fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,6 @@ BigNumber.config({
 })
 
 export const BSC_BLOCK_TIME = 3
-export const CAKE_POOL_PID = 1
 
 // CAKE_PER_BLOCK details
 // 40 CAKE is minted per block
@@ -18,7 +17,6 @@ export const CAKE_POOL_PID = 1
 
 export const CAKE_PER_BLOCK = new BigNumber(40)
 export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
->>>>>>> e9aecddb7a0deb5c49b28103db1e18845263fd63
 export const BASE_URL = 'https://app.stableflect.finance'
 export const BASE_EXCHANGE_URL = 'https://app.stableflect.finance'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_EXCHANGE_URL}/#/add`
